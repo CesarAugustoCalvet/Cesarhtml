3d3325ce1360e9979c128b2e3017d1267e6e35e5
CesarAugustoCalvet <120611865+CesarAugustoCalvet@users.noreply.github.com>
2023-03-29 13:27:15 -0300

    Testando Condições Multiplas no JS

--- teste01.js
+++ teste01.js
@@ -1,1 +1,8 @@
-console.log('Olá Mundo')
+var idade = 72
+if (idade < 16) { 
+console.log ( "Não Vota" ) 
+}  else if ( idade < 18 || idade > 70) { 
+console.log ("Voto Opcional")
+} else if ( idade > 18  <= 70) {
+console.log ("Voto Obrigatório") 
+} 
