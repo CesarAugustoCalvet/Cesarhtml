8d06d6d0a60125e04cad99addc1ae66fb8d8db68
CesarAugustoCalvet <120611865+CesarAugustoCalvet@users.noreply.github.com>
2023-05-17 20:29:07 -0300

    Teste de JS

---
+++ TestesJS/primeiroteste.js
@@ -0,0 +1,18 @@
+console.log("Bloco Impar")
+{
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+    console.log(9)
+    console.log("Fim do Bloco Impar")
+}
+console.log("Bloco Par")
+{
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+    console.log("Fim do Bloco Par")
+}
