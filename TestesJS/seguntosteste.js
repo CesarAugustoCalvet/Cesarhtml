1643bc164a63d89c7977b4a07e472cc07a888032
CesarAugustoCalvet <120611865+CesarAugustoCalvet@users.noreply.github.com>
2023-05-29 21:59:41 -0300

    terceiro teste de JS

---
+++ TestesJS/seguntosteste.js
@@ -0,0 +1,4 @@
+let raio = 10
+const pi = 3.141592
+let AreaCircu = pi * raio * raio
+console.log(" O resultado da Área de Circunferência é de : " + AreaCircu + " m2")
---
+++ TestesJS/terceiroteste.js
@@ -0,0 +1,13 @@
+{let a = 30
+    let b = 40
+    console.log(a)
+    console.log(b)
+    console.log("Variaveis a e b com valores originais!")
+
+}
+
+{let a = 40
+    let b = 30 
+    console.log(a)
+    console.log(b)
+    console.log("Variaveis a e b com valores trocados!")}
