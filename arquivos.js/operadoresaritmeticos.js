8a740970eebb111327ce879456d69576dbd04cf8
CesarAugustoCalvet <120611865+CesarAugustoCalvet@users.noreply.github.com>
2023-08-09 18:28:20 -0300

    Novos Arquivos JS

---
+++ arquivos.js/operadoresaritmeticos.js
@@ -0,0 +1,6 @@
+const [a,b,c,d] = [3,10,15,20]
+const soma = a + b + c + d 
+const subtracao = d - c 
+const multiplicacao = a * b 
+const divisao = d / b
+console.log(soma,subtracao,multiplicacao,divisao)
