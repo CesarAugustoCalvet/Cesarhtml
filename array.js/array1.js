f47303a48254270ef6fc66289817c09a5858bf1e
CesarAugustoCalvet <120611865+CesarAugustoCalvet@users.noreply.github.com>
2024-03-23 10:49:18 -0300

    Exercícios de JS
    Segue para o Github mais alguns exercícios de JS

---
+++ array.js/array1.js
@@ -0,0 +1,15 @@
+let aprovados = new Array (`Cesar`,`Lílian`, `João` )
+console.log(aprovados)
+aprovados = [`Cesar`, `Lílian`, `João`]
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
+// Diferentes Formas de Adicionar um novo elemento ao Array
+aprovados[3] = `Celso`
+aprovados.push(`Katia`)
+console.log(aprovados.length)
+aprovados[10] = `Fernando`
+console.log(aprovados)
+
+
