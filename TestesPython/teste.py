e4ef472bb18f77cafb369e2f6b5fd38e08030c63
CesarAugustoCalvet <120611865+CesarAugustoCalvet@users.noreply.github.com>
2024-07-01 15:26:33 -0300

    Arquivos de Python
    Começando a aprender outra linguagem !

---
+++ TestesPython/teste.py
@@ -0,0 +1,14 @@
+
+print('Qual é o seu nome?' , 'Cesar')
+print('Olá Cesar ! ' , 'Seja bem vindo!')
+dia = 21 
+mes = 'Dezembro'
+ano = 1992
+print('Você nasceu ', 'no', dia , ' de', mes, ' de', ano )
+numero1 = 10
+numero2 = 20
+soma = numero1 + numero2
+print("Digite um primeiro número :" , numero1)
+print("Digite um segundo número :" , numero2)
+print(" A soma dos dois números é : " , soma)
+
