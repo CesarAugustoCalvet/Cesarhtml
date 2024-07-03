0b170bf34e9599ce2c8d64bc774ea162f1b4f535
CesarAugustoCalvet <120611865+CesarAugustoCalvet@users.noreply.github.com>
2024-07-03 19:23:05 -0300

    Mais Arquivos de Python
    Segue mais um Arquivo de Python

---
+++ TestesPython/teste02.py
@@ -0,0 +1,10 @@
+# Tipos Primitivos de Dados : int=> número inteiro, float => número real, bool => valores booleanos(verdadeiro/falso), str=> valores textuais.
+#n1 = int(input('Digite um número:')) 
+#n2 =  int (input('Digite mais um número:'))
+#soma = n1 + n2
+#print('A soma dos dois números é: ', soma)
+
+num1 = int(input('digite o primeiro número: '))
+num2 = int(input('digite o segundo número: '))
+soma = num1 + num2
+print(' A soma de {} e {} é igual a {}'.format(num1,num2,soma))
