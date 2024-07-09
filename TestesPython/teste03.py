40dfc79a1042cce3a1a2cbe9fc7d7304e1b52f82
CesarAugustoCalvet <120611865+CesarAugustoCalvet@users.noreply.github.com>
2024-07-08 23:14:56 -0300

    Arquivos Python
    Mais exercícios de Python

---
+++ TestesPython/teste03.py
@@ -0,0 +1,9 @@
+num1 = float(input("Digite o primeiro número : "))
+antecessor = num1 - 1
+sucessor = num1 + 1
+dobro = num1*2
+triplo = num1*3
+raiz = num1**(1/2)
+potencia = num1**2
+print(" o número digitado : {} , seu antecessor é {}, seu sucessor é {}, " .format(num1,antecessor,sucessor))
+print(" o dobro dele é {} , o triplo é {}, sua raiz é {}, sua potência é {}".format(dobro,triplo,raiz,potencia))
---
+++ TestesPython/teste04.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro número : "))
+num2 = int(input("Digite o segundo número "))
+soma = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+media = (num1 + num2) / 2
+print("A soma dos números é : {} , A subtração dos números é : {} ".format(soma,sub))
+print("A multiplicação dos números é : {} , A divisão dos números é : {}, A média dos números é : {}".format(mult,div,media))
